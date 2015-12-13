ac38c294477373813b929dcebd0a4ad5732e77f9
solahyoo <tjf009@berkeley.edu>
2015-12-12 19:40:57 -0800

    updated data

--- code/cleaning_data.R
+++ code/cleaning_data.R
@@ -13,8 +13,8 @@ colnames(gdp_data) <- c('x', 'iso2c', 'country', 'year',
                         'GDP per capita (current US$)',
                         'GDP per capita growth (annual %)')
 
-# Only need years 1970 - current
-gdp_data <- subset(gdp_data, year >= 1970)
+# Only need years 1980 - current
+gdp_data <- subset(gdp_data, year >= 1980)
 
 # Renumber rows
 row.names(gdp_data) <- 1:nrow(gdp_data)
@@ -24,6 +24,7 @@ gdp_data <- gdp_data[c(3:10)]
 
 # Export to csv file
 write.csv(file = '/data/gdp_data.csv', x = gdp_data, row.names = FALSE)
+
 
 # Clean Box Office data
 box_office <- read.csv(file = '/rawdata/raw_box_office.csv')
@@ -39,28 +40,40 @@ box_office <- box_office[c(1, 2, 4, 6:8)]
 box_office$Total.Gross <- box_office$Total.Gross * 1000000
 box_office$Tickets.Sold <- box_office$Tickets.Sold * 1000000
 
+# Don't need year 2015
+box_office <- subset(box_office, Year < 2015)
+
+# Reorder data so that Year 1980 is on top
+box_office <- box_office[order(box_office$Year), ]
+
 # Write to csv file
 write.csv(file = '/data/box_offce.csv', x = box_office, row.names = FALSE)
+
 
 # Clean data for top grossing movie of the year
 top_movie <- read.csv(file = '/rawdata/raw_top_grossing_movie_yearly.csv',
                       stringsAsFactors = FALSE)
-top_movie <- subset(top_movie, Year >= 1970 & Year < 2015)
+top_movie <- subset(top_movie, Year >= 1980 & Year < 2015)
 top_movie <- top_movie[c("Year", 'Movie', 'Genre', 'Production.Budget',
                          'Total.Domestic.Box.Office')]
 # Convert budget to numeric
 budget <- top_movie$Production.Budget
 top_movie$Production.Budget <- as.numeric(gsub(",", "",
                                                gsub("\\$", "", budget)))
+
 # Convert total box office to numeric
 total_box_office <- top_movie$Total.Domestic.Box.Office
 top_movie$Total.Domestic.Box.Office <- as.numeric(gsub(",", "",
                                         gsub("\\$", "", total_box_office)))
+# Reorder rows
+top_movie <- top_movie[order(top_movie$Year), ]
+
 # Renumber rows
 row.names(top_movie) <- 1:nrow(top_movie)
 
 # Write to csv file
-write.csv(file = '/data/top_grossing_movie_yearly.csv', x = top_movie, row.names = FALSE)
+write.csv(file = '/data/top_grossing_movie_yearly.csv',
+          x = top_movie, row.names = FALSE)
 
 
 
